74119fd9315510b5cfad1f0ef3f63e21d25a1f5c
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-15 13:47:15 -0600

    display cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
-const primaryColor = Color.fromRGBO(175, 180, 174, 1.0);
+const primaryColor = Color.fromRGBO(124, 157, 150, 1.0);
 const primaryColorAccent = Color.fromRGBO(210, 215, 209, 1.0);
 
 class BMICalculator extends StatelessWidget {
@@ -21,18 +21,171 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  int _weight = 0;
+  int _height = 0;
+  int _age = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('BODY TEXT'),
-      ),
-      floatingActionButton: TextButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Icon(
+                            Icons.male,
+                            size: 50.0,
+                          ),
+                        ),
+                        Expanded(
+                          child: Text("MALE"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Icon(
+                            Icons.female,
+                            size: 50.0,
+                          ),
+                        ),
+                        Expanded(
+                          child: Text("FEMALE"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text("HEIGHT"),
+                        ),
+                        Expanded(
+                          child: Text("$_height cm"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text("WEIGHT"),
+                        ),
+                        Expanded(
+                          child: Text(
+                            "$_weight",
+                            style: TextStyle(fontSize: 50.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text("AGE"),
+                        ),
+                        Expanded(
+                          child: Text("$_age"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateColor.resolveWith((states) =>
+                    (states == MaterialState.pressed ||
+                            states == MaterialState.focused)
+                        ? Color.fromRGBO(255, 139, 68, 1.0)
+                        : Color.fromRGBO(233, 179, 132, 1)),
+                shape: MaterialStateProperty.all(
+                  ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                    // side: BorderSide(width: double.infinity)
+                  ),
+                ),
+                minimumSize: MaterialStateProperty.resolveWith(
+                  (states) => Size(
+                    double.infinity,
+                    50.0,
+                  ),
+                ),
+                textStyle: MaterialStateTextStyle.resolveWith(
+                  (states) => TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              child: Text("CALCULATE"),
+            ),
+          )
+        ],
       ),
     );
   }
