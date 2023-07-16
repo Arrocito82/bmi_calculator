4db1092cc034d96b48c50b53bc4c8101007399f9
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-16 12:05:54 -0600

    Layout different for portrait and landscape position

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -14,139 +14,192 @@ class _InputPageState extends State<InputPage> {
   int _height = 0;
   int _age = 0;
   int _gender = 0;
+
+  List<Widget> genderButtons = [
+    Expanded(
+      flex: 3,
+      child: MyCard(
+        primaryColor: primaryColor,
+        child: IconContent(
+          icon: Icons.male,
+          text: "MALE",
+        ),
+      ),
+    ),
+    Expanded(
+      flex: 3,
+      child: MyCard(
+        primaryColor: primaryColor,
+        child: IconContent(
+          icon: Icons.female,
+          text: "FEMALE",
+        ),
+      ),
+    ),
+  ];
+  Expanded getHeigthCard() => Expanded(
+        flex: 4,
+        child: MyCard(
+          primaryColor: primaryColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("HEIGHT"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "$_height",
+                    style: TextStyle(fontSize: 50.0),
+                  ),
+                  Text(
+                    " cm",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+  Widget getCalculateButton1() => Expanded(
+        flex: 2,
+        child: GestureDetector(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(0),
+              color: primaryColor,
+            ),
+            width: double.infinity,
+            child: Center(
+              child: Text(
+                "CALCULATE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+  TextButton getCalculateButton2() => TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          backgroundColor: MaterialStateColor.resolveWith((states) =>
+              (states == MaterialState.pressed ||
+                      states == MaterialState.focused ||
+                      states == MaterialState.selected)
+                  ? Color.fromRGBO(255, 139, 68, 1.0)
+                  : Color.fromRGBO(233, 179, 132, 1)),
+          shape: MaterialStateProperty.all(
+            ContinuousRectangleBorder(
+              borderRadius: BorderRadius.circular(0),
+              // side: BorderSide(width: double.infinity)
+            ),
+          ),
+          // minimumSize: MaterialStateProperty.resolveWith(
+          //   (states) => Size(
+          //     double.infinity,
+          //     50.0,
+          //   ),
+          // ),
+        ),
+        child: Text(
+          "CALCULATE",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      );
+  List<Widget> getWeightAgeButtons() => [
+        Expanded(
+          flex: 3,
+          child: MyCard(
+            primaryColor: primaryColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("WEIGHT"),
+                Text(
+                  "$_weight",
+                  style: TextStyle(fontSize: 50.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: MyCard(
+            primaryColor: primaryColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("AGE"),
+                Text(
+                  "$_age",
+                  style: TextStyle(fontSize: 50.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-            crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
-            children: [
-              Wrap(
-                direction: Axis.horizontal,
+      body: Container(
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            if (orientation == Orientation.portrait) {
+              return Column(
                 children: [
                   Row(
-                    children: [
-                      Expanded(
-                        child: MyCard(
-                          primaryColor: primaryColor,
-                          child: IconContent(
-                            icon: Icons.male,
-                            text: "MALE",
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: MyCard(
-                          primaryColor: primaryColor,
-                          child: IconContent(
-                            icon: Icons.female,
-                            text: "FEMALE",
-                          ),
-                        ),
-                      ),
-                    ],
+                    children: genderButtons,
                   ),
-                  MyCard(
-                    primaryColor: primaryColor,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("HEIGHT"),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "$_height",
-                              style: TextStyle(fontSize: 50.0),
-                            ),
-                            Text(
-                              " cm",
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                  getHeigthCard(),
+                  Row(
+                    children: getWeightAgeButtons(),
                   ),
+                  getCalculateButton1(),
                 ],
-              ),
-              Wrap(
-                direction: Axis.horizontal,
+              );
+            } else {
+              return Row(
                 children: [
                   Expanded(
-                    child: Row(
+                    child: Column(
                       children: [
                         Expanded(
-                          child: MyCard(
-                            primaryColor: primaryColor,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("WEIGHT"),
-                                Text(
-                                  "$_weight",
-                                  style: TextStyle(fontSize: 50.0),
-                                ),
-                              ],
-                            ),
+                          child: Row(
+                            children: genderButtons,
                           ),
                         ),
                         Expanded(
-                          child: MyCard(
-                            primaryColor: primaryColor,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("AGE"),
-                                Text(
-                                  "$_age",
-                                  style: TextStyle(fontSize: 50.0),
-                                ),
-                              ],
-                            ),
+                          child: Row(
+                            children: getWeightAgeButtons(),
                           ),
                         ),
                       ],
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateColor.resolveWith(
-                          (states) => (states == MaterialState.pressed ||
-                                  states == MaterialState.focused ||
-                                  states == MaterialState.selected)
-                              ? Color.fromRGBO(255, 139, 68, 1.0)
-                              : Color.fromRGBO(233, 179, 132, 1)),
-                      shape: MaterialStateProperty.all(
-                        ContinuousRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                          // side: BorderSide(width: double.infinity)
-                        ),
-                      ),
-                      // minimumSize: MaterialStateProperty.resolveWith(
-                      //   (states) => Size(
-                      //     double.infinity,
-                      //     50.0,
-                      //   ),
-                      // ),
-                    ),
-                    child: Text(
-                      "CALCULATE",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        getHeigthCard(),
+                        getCalculateButton1(),
+                      ],
                     ),
                   ),
                 ],
-              )
-            ],
-          );
-        },
+              );
+            }
+          },
+        ),
       ),
     );
   }
